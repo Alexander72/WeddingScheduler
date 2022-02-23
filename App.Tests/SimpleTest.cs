79a9d79e539726544f1e1faaa27a1aca124fba93
1tsova <1tsovai@gmail.com>
2022-02-23 23:31:40 +0100

    wedding service tests + some updates

--- App.Tests/SimpleTest.cs
+++ App.Tests/SimpleTest.cs
@@ -1,13 +1,25 @@
 using Xunit;
+using System.Collections.Generic;
+using App.Models;
 
 namespace App.Tests
 {
     public class SimpleTest
     {
-        [Fact]
+       /* [Fact]
         public void testSomething()
         {
             Assert.False(false);
         }
+        */
+
+        [Fact]
+        public void guestName()
+        {
+            Guest guest = new Guest("Inga", new List<int>{1, 3});
+            Assert.True(guest.isWorkingAt(3));
+        }
+
+
     }
 }
